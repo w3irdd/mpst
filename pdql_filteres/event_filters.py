20037047a951deb7d1fb7e87f3f450de41a23c25
w3irdd <sergei.ilin1906@ya.ru>
2024-06-26 15:14:38 +0300

    status field fix

--- pdql_filteres/event_filters.py
+++ pdql_filteres/event_filters.py
@@ -17,38 +17,11 @@ fortinet_attacks = {
             "dst.host",
             "dst.ip",
             "dst.port",
-            "status",
+            "object.state",
             "object.type",
             "text",
             "uuid"
     ],
     "top": None,
     "where": "(event_src.vendor = \"fortinet\") AND (msgid = \"0419016384\")"
-}
-
-vpn_connects = {
-    "aggregateBy": [],
-    "aliases": None,
-    "distributeBy": [],
-    "groupBy": [],
-    "orderBy": [
-        {
-            "field": "time",
-            "sortOrder": "descending"
-        }
-    ],
-    "select": [
-            "time",
-            "event_src.host",
-            "src.ip",
-            "src.geo.country",
-            "dst.host",
-            "dst.ip",
-            "dst.port",
-            "status",
-            "object.type",
-            "text"
-    ],
-    "top": None,
-    "where": "(task_id = \"1a25286e-9600-0001-0000-000000000006\") AND (recv_ipv4 = \"10.100.3.26\") AND (msgid = \"722051\")"
 }
